e3ce0886f2e720fbe2d496b6580c1bfe2ffc743f
manasgupta26 <manasg2698@gmail.com>
2020-09-08 13:54:20 +0530

    Lifestyle Store

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+First create a database name ecommerce in PHPmyadmin and then import the sql file from the folder Database. 
---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+    include 'includes/common.php';
+?>
+
+<footer>
+	<div class="container">
+		<center>
+			<p>Copyright &copy; Lifestyle Store. All Rights Reserved | Contact Us: +91 9000000000</p>
+		</center>
+	</div>
+</footer>
